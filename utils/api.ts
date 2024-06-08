0ae14c79ce7afcad152e55458953cbbeb60b5bdc
Javier Robles <javiroblesfernandez@gmail.com>
2024-06-08 23:07:31 +0200

    Refactor

--- utils/api.ts
+++ utils/api.ts
@@ -4,6 +4,7 @@ import * as fields from "$lib/cms.config";
 import { prisma } from "$lib/core/utils";
 import type { Action, RequestHandler, ServerLoad } from "@sveltejs/kit";
 import { json } from "@sveltejs/kit";
+import type { Entry } from "./types";
 
 export const setupGET =
 	(table: keyof typeof fields): RequestHandler =>
@@ -14,7 +15,7 @@ export const setupGET =
 export const setupLoad =
 	(table: keyof typeof fields) =>
 	async ({ platform }: Parameters<ServerLoad>["0"]) => {
-		return { entries: await (prisma(platform)[table] as any).findMany() };
+		return { entries: (await (prisma(platform)[table] as any).findMany()) as Entry[] };
 	};
 
 export const setupActions = (table: keyof typeof fields) => ({
